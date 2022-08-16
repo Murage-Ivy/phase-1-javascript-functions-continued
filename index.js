d48caa58191c1743f5457e2c6ead6dc061683969
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-08-16 10:32:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+// function declaration
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+console.log(saturdayFun());
+
+//function expression
+const mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+console.log(mondayWork());
+
+//function scope-level
+
+function wrapAdjective(Vflair = "*") {
+    return function (adjective = 'special') {
+        return `You are ${Vflair}${adjective}${Vflair}!`;
+    }
+}
